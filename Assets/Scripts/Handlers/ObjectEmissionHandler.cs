0f5310417d6188c0d7cacaa566b42dc2b53df892
armandpreschutti <armand.preschutti@gmail.com>
2023-08-11 16:35:30 -0400

    Removed "Camera" system
    Decided to remove the dynamic camera system and instead set the camera position to be static in an effort to balance gameplay.

--- Assets/Scripts/Handlers/ObjectEmissionHandler.cs
+++ Assets/Scripts/Handlers/ObjectEmissionHandler.cs
@@ -4,49 +4,33 @@ using UnityEngine;
 using static UnityEngine.UI.Image;
 using UnityEngine.UIElements;
 using Unity.VisualScripting;
-
 using DG.Tweening.Core.Easing;
 
 public class ObjectEmissionHandler : MonoBehaviour
 {
-    public GameObject player;
-    public GameObject prefabToSpawn;
+    
     public BoxCollider2D spawnArea;
-    //private bool isLooping = false;
-    public float spawnTimeRate;
-   // public float spawnRateMultiplier;
-    public float spawnRateThreshold;
+
+    public GameObject prefabToSpawn;
+    public float spawnRate;
+
 
     /// <summary>
     /// On start, this function is called
     /// </summary>
     void OnEnable()
     {
-        SetComponents();
+        spawnArea = GetComponent<BoxCollider2D>();
         StartCoroutine(RepeatSpawning());
     }
 
-    public void Update()
-    {
-        this.transform.position = new Vector3(20, player.transform.position.y,0);
-    }
-   
     /// <summary>
     /// When called, this coroutine spawns a object then waits a certain amount of time before respawning again.
     /// </summary>
     /// <returns></returns>
     private IEnumerator RepeatSpawning()
     {
-        
-        /* if(spawnTimeRate >= spawnRateThreshold)
-         {
-             spawnTimeRate = spawnTimeRate * spawnRateMultiplier;
-         }
-         else
-         {
-             yield return new WaitForSeconds(spawnTimeRate);
-         }*/
-        yield return new WaitForSeconds(spawnTimeRate);
+        yield return new WaitForSeconds(spawnRate);
         SpawnPrefab();
         StartCoroutine(RepeatSpawning());
     }
@@ -62,30 +46,6 @@ public class ObjectEmissionHandler : MonoBehaviour
 
         // Create an instance of prefab at spawn position
         GameObject spawnedObject = Instantiate(prefabToSpawn, spawnPosition, Quaternion.identity);
-
     }
 
-    /// <summary>
-    /// When called, this function sets all components needed
-    /// </summary>
-    public void SetComponents()
-    {
-        spawnArea = GetComponent<BoxCollider2D>();
-        player = GameObject.Find("Player");
-    }
-
-    /*
-        public void StartLooping()
-        {
-            if (!isLooping)
-            {
-                isLooping = true;
-                StartCoroutine(MyCoroutine());
-            }
-        }
-
-        public void StopLooping()
-        {
-            isLooping = false;
-        }*/
 }
